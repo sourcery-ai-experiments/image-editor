8122d6b2feab2f737604345f75a29a6ac1570743
NaveedSahotra555 <naveed.sahotra3@gmail.com>
2024-05-09 13:35:40 +0500

    api-integrated and on canvas added through click and dnd

---
+++ src/api/text-to-image/index.ts
@@ -0,0 +1,33 @@
+import { BaseURL } from "../../constants";
+
+
+
+export async function textToImage(prompt: string) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    prompt: prompt,
+    dall_e_version: "dall-e-3",
+  });
+
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      `${BaseURL}/api/get-images`,
+      requestOptions
+    );
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
