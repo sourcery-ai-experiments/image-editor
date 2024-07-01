d1acb743793932014f83c3bc12dc7f4ac1ef9ef2
asad195 <81696767+Asad195@users.noreply.github.com>
2024-07-01 16:28:16 +0500

    fix: bubble fixes

--- src/utils/BubbleHandler.ts
+++ src/utils/BubbleHandler.ts
@@ -272,8 +272,25 @@ export const createBubbleElement1 = (
 					radius: strokeCircle.radius!,
 				})
 				.setCoords();
-		});
 
+			// Update the image scale and position
+			const newScaleFactor = Math.max(
+				strokeCircle.getScaledWidth() / fabricImage.width!,
+				strokeCircle.getScaledHeight() / fabricImage.height!
+			);
+
+			fabricImage
+				.set({
+					scaleX: newScaleFactor,
+					scaleY: newScaleFactor,
+					left: fabricImage.left,
+					top: fabricImage.top,
+				})
+				.setCoords();
+
+			prevLeft = strokeCircle.left!;
+			prevTop = strokeCircle.top!;
+		});
 		canvas.renderAll();
 	};
 };
